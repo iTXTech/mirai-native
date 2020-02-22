61c5518f7809c1431654e5488b23a1b1dae81742
PeratX <1215714524@qq.com>
2020-02-22 12:21:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Mirai Native
+
+Mirai Native 致力于通过实现 酷Q 的兼容API使 酷Q 的应用（插件）能在 [Mirai](https://github.com/mamoe/mirai) 环境下运行。
+
+## 技术路线
+
+* Kotlin
+* JNI
+
+## 开源协议
+
+    Copyright (C) 2020 iTX Technologies
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ src/main/kotlin/org/itxtech/mirai/native/MiraiNative.kt
@@ -0,0 +1,36 @@
+package org.itxtech.mirai.native
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import net.mamoe.mirai.console.plugins.PluginBase
+import net.mamoe.mirai.utils.MiraiExperimentalAPI
+
+/*
+ *
+ * Mirai Native
+ *
+ * Copyright (C) 2020 iTX Technologies
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author PeratX
+ * @website https://github.com/iTXTech/mirai-native
+ *
+ */
+class MiraiNative : PluginBase() {
+    @ExperimentalCoroutinesApi
+    @MiraiExperimentalAPI
+    override fun onEnable() {
+        logger.info("Mirai Native enabled, loading DLLs.")
+    }
+}
