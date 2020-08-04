8ec4474fdfab66a1786238a5616c9671b9b75dd8
PeratX <1215714524@qq.com>
2020-08-05 00:02:38 +0800

    mirai: update

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,9 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
 
-    implementation("net.mamoe:mirai-core:1.0.2")
+    implementation("net.mamoe:mirai-core:1.1.3")
     implementation("net.mamoe:mirai-console:0.5.2")
 }
 
