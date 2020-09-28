c0a3fad58876b2c3c59487bc27eba8c8d720f475
PeratX <1215714524@qq.com>
2020-09-28 09:54:33 +0800

    MiraiNative: upload to bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.serialization") version "1.4.10"
+    id("com.jfrog.bintray") version "1.8.5"
+    `maven-publish`
 }
 
 group = "org.itxtech"
 version = "1.9.2"
+description = "强大的 mirai 原生插件加载器。"
+val vcs = "https://github.com/iTXTech/mirai-native"
 
 kotlin {
     sourceSets {
@@ -38,4 +42,46 @@ tasks.named<Jar>("jar") {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+bintray {
+    user = property("buser") as String?
+    key = property("bkey") as String?
+    setPublications("mavenJava")
+    setConfigurations("archives")
+    pkg.apply {
+        repo = "mirai"
+        name = "mirai-native"
+        userOrg = "itxtech"
+        setLicenses("AGPLv3")
+        publicDownloadNumbers = true
+        vcsUrl = vcs
+    }
+}
+
+@Suppress("DEPRECATION")
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+
+            groupId = rootProject.group.toString()
+            this.artifactId = artifactId
+            version = version
+
+            pom.withXml {
+                val root = asNode()
+                root.appendNode("description", description)
+                root.appendNode("name", project.name)
+                root.appendNode("url", vcs)
+                root.children().last()
+            }
+            artifact(sourcesJar.get())
+        }
+    }
 }
